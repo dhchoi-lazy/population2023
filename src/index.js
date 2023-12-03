5add8738a882695a2544eaef4d504dea35bc9681
dhchoi-lazy <135495811+dhchoi-lazy@users.noreply.github.com>
2023-12-03 17:57:56 +0900

    change url

--- src/index.js
+++ src/index.js
@@ -3,48 +3,40 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Cartogram from "./components/Cartogram";
-import { BrowserRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider, Outlet } from "react-router-dom";
 
-// import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <App>
-//         <Cartogram bgColor={"black"} />
-//         <Outlet />
-//       </App>
-//     ),
-//     children: [
-//       {
-//         path: "/cartogram-sbs",
-//         element: (
-//           <App>
-//             <Cartogram bgColor={"black"} />
-//           </App>
-//         ),
-//       },
-//       {
-//         path: "/cartogram-mbc",
-//         element: (
-//           <App>
-//             <Cartogram bgColor={"white"} />
-//           </App>
-//         ),
-//       },
-//     ],
-//   },
-//   {
-//     basename: "/population2023",
-//   },
-// ]);
+const router = createHashRouter([
+  {
+    path: "/cartogram-sbs",
+    element: (
+      <App>
+        <Cartogram bgColor={"black"} />
+        <Outlet />
+      </App>
+    ),
+    children: [
+      {
+        path: "/cartogram-sbs",
+        element: (
+          <App>
+            <Cartogram bgColor={"black"} />
+          </App>
+        ),
+      },
+      {
+        path: "/cartogram-mbc",
+        element: (
+          <App>
+            <Cartogram bgColor={"white"} />
+          </App>
+        ),
+      },
+    ],
+  },
+  {
+    basename: "/population2023",
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<RouterProvider router={router} />);
-root.render(
-  <BrowserRouter basename="/population2023">
-    <Link to="/cartogram-sbs">Cartogram SBS</Link>
-    <Link to="/cartogram-sbs">Cartogram SBS</Link>
-  </BrowserRouter>
-);
+root.render(<RouterProvider router={router} />);
