cc39e8ade43520036eca37de1ffebd282b991cf8
dhchoi-lazy <135495811+dhchoi-lazy@users.noreply.github.com>
2023-12-03 17:06:09 +0900

    change route method

--- src/index.js
+++ src/index.js
@@ -5,26 +5,31 @@ import App from "./App";
 import Cartogram from "./components/Cartogram";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
+// Creating the router with the proper basename
+const router = createBrowserRouter(
+  [
+    {
+      path: "/cartogram-sbs",
+      element: (
+        <App>
+          <Cartogram bgColor={"black"} />
+        </App>
+      ),
+    },
+    {
+      path: "/cartogram-mbc",
+      element: (
+        <App>
+          <Cartogram bgColor={"white"} />
+        </App>
+      ),
+    },
+  ],
   {
-    path: "/cartogram-sbs",
+    // Setting the basename for all routes
     basename: "/population2023",
-    element: (
-      <App>
-        <Cartogram bgColor={"black"} />
-      </App>
-    ),
-  },
-  {
-    path: "/cartogram-mbc",
-    basename: "/population2023",
-    element: (
-      <App>
-        <Cartogram bgColor={"white"} />
-      </App>
-    ),
-  },
-]);
+  }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
