92e7e905563fcf550562c6fcff04d0ee3102b59e
dhchoi-lazy <135495811+dhchoi-lazy@users.noreply.github.com>
2023-12-03 16:36:13 +0900

    change url

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Cartogram from "./components/Cartogram";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      {/* Direct URL Links */}
+      <div>
+        <Link to="/cartogram">Go to Cartogram Black</Link>
+      </div>
+      <div>
+        <Link to="/cartogram/mbc">Go to Cartogram White</Link>
+      </div>
+
+      {/* Routes */}
       <Routes>
+        <Route path="/cartogram" element={<Cartogram bgColor={"black"} />} />
         <Route
-          path="/cartogram"
-          element={<Cartogram bgColor={"black"} />}
-        ></Route>
-        <Route>
-          <Route
-            path="/cartogram/mbc"
-            element={<Cartogram bgColor={"white"} />}
-          ></Route>
-        </Route>
+          path="/cartogram/mbc"
+          element={<Cartogram bgColor={"white"} />}
+        />
       </Routes>
     </div>
   );
