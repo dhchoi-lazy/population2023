1c0e0c4307dc7e654561cdb184650b3e04a99b4b
dhchoi-lazy <135495811+dhchoi-lazy@users.noreply.github.com>
2023-12-03 17:42:46 +0900

    change url

--- src/index.js
+++ src/index.js
@@ -3,31 +3,36 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Cartogram from "./components/Cartogram";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/cartogram-sbs",
-      element: (
-        <App>
-          <Cartogram bgColor={"black"} />
-        </App>
-      ),
-    },
-    {
-      path: "/cartogram-mbc",
-      element: (
-        <App>
-          <Cartogram bgColor={"white"} />
-        </App>
-      ),
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: "/population2023",
-  }
-);
+    element: (
+      <App>
+        <Cartogram bgColor={"black"} />
+        <Outlet />
+      </App>
+    ),
+    children: [
+      {
+        path: "cartogram-sbs",
+        element: (
+          <App>
+            <Cartogram bgColor={"black"} />
+          </App>
+        ),
+      },
+      {
+        path: "cartogram-mbc",
+        element: (
+          <App>
+            <Cartogram bgColor={"white"} />
+          </App>
+        ),
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
